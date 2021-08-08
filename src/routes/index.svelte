81cbe4669549b1dd2a34c37e303d6ead791bc1af
Julia Howes <juliahowes@Julias-MacBook-Air.local>
2021-08-08 11:13:53 -0700

    routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,14 +37,8 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Julia Howes</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<h1>Julia Howes</h1>
+<h2>Software Engineer</h2>
---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+
+</script>
+
+<script>
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+	<title>Projects</title>
+</svelte:head>
+
+<h1>Projects</h1>
